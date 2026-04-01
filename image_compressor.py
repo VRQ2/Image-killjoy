cf284a3bb8a4b184ecc6cc9b7df9c6923e1e0d15
VRQ2 <mateusz.lukasik@student.pk.edu.pl>
2026-04-01 10:45:23 +0200

    Added andomizer for image filter

--- image_compressor.py
+++ image_compressor.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageFilter, ImageOps, ImageEnhance
 from tkinter.filedialog import askopenfilename, askdirectory
 import os
+import random
 
 #file_to_convert = askopenfilename()
 
@@ -10,8 +11,6 @@ directory_to_save = askdirectory()
 
 #input_img = Image.open(file_to_convert)
 
-#TODO: Add loop for every image in directory
-#TODO: Randomizer for image filter
 #TODO: Adjust filters
 #TODO: Simple console GUI (1.degrade one, 2.degrade folder, 3.exit)
 
@@ -27,5 +26,18 @@ print(directory_to_save)
 
 for e in range(len(files)):
     input_img = Image.open(f"{directory_to_convert}/{files[e]}")
-    output_img = ImageEnhance.Contrast(input_img).enhance(1.7)
+    seed = random.randrange(4)
+    match seed:
+        case 0:
+            output_img = ImageEnhance.Contrast(input_img).enhance(1.7)
+
+        case 1:
+            output_img = input_img.filter(ImageFilter.GaussianBlur(1.8))
+
+        case 2:
+            output_img = ImageOps.solarize(input_img, 150)
+
+        case 3:
+            output_img = ImageOps.posterize(input_img, bits=2)
+
     output_img.save(f'{directory_to_save}/changed{e}.jpg')
