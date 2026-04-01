4053f9799ca0df6ffd9fc31ce3521f1a379588b7
VRQ2 <mateusz.lukasik@student.pk.edu.pl>
2026-04-01 10:32:08 +0200

    Add loop for every image in directory

--- image_compressor.py
+++ image_compressor.py
@@ -1,10 +1,14 @@
 from PIL import Image, ImageFilter, ImageOps, ImageEnhance
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilename, askdirectory
 import os
 
-file_to_convert = askopenfilename()
+#file_to_convert = askopenfilename()
 
-input_img = Image.open(file_to_convert)
+directory_to_convert = askdirectory()
+
+directory_to_save = askdirectory()
+
+#input_img = Image.open(file_to_convert)
 
 #TODO: Add loop for every image in directory
 #TODO: Randomizer for image filter
@@ -14,7 +18,14 @@ input_img = Image.open(file_to_convert)
 #output_img = input_img.filter(ImageFilter.GaussianBlur(1.8))
 #output_img = ImageOps.posterize(input_img, bits=2)
 #output_img = ImageOps.solarize(input_img, 150)
-output_img = ImageEnhance.Contrast(input_img).enhance(1.7)
+#output_img = ImageEnhance.Contrast(input_img).enhance(1.7)
 
-output_img.save('test.jpg')
-output_img.show()
+files = [f for f in os.listdir(directory_to_convert) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
+print(files)
+print(directory_to_convert)
+print(directory_to_save)
+
+for e in range(len(files)):
+    input_img = Image.open(f"{directory_to_convert}/{files[e]}")
+    output_img = ImageEnhance.Contrast(input_img).enhance(1.7)
+    output_img.save(f'{directory_to_save}/changed{e}.jpg')
