ea81461541713639fda4d75c750cc28f1ebd67de
VRQ2 <mateusz.lukasik@student.pk.edu.pl>
2026-04-01 11:50:09 +0200

    Adjusted filters

--- image_compressor.py
+++ image_compressor.py
@@ -11,7 +11,6 @@ directory_to_save = askdirectory()
 
 #input_img = Image.open(file_to_convert)
 
-#TODO: Adjust filters
 #TODO: Simple console GUI (1.degrade one, 2.degrade folder, 3.exit)
 
 #output_img = input_img.filter(ImageFilter.GaussianBlur(1.8))
@@ -29,15 +28,15 @@ for e in range(len(files)):
     seed = random.randrange(4)
     match seed:
         case 0:
-            output_img = ImageEnhance.Contrast(input_img).enhance(1.7)
+            output_img = ImageEnhance.Contrast(input_img).enhance(random.uniform(0.5,1.4))
 
         case 1:
-            output_img = input_img.filter(ImageFilter.GaussianBlur(1.8))
+            output_img = input_img.filter(ImageFilter.GaussianBlur(random.uniform(1.7,2)))
 
         case 2:
-            output_img = ImageOps.solarize(input_img, 150)
+            output_img = ImageOps.solarize(input_img, random.randint(180,240))
 
         case 3:
-            output_img = ImageOps.posterize(input_img, bits=2)
+            output_img = ImageOps.posterize(input_img, bits=random.randint(1,2))
 
     output_img.save(f'{directory_to_save}/changed{e}.jpg')
